5f4870988fafb4ff55ca1d61b098ba168272dbce
cypmars <cypconnet@wanadoo.fr>
2017-04-02 22:57:38 +0200

    --------------V0.0.3----------------

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -1,19 +1,16 @@
 package hello;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URI;
-import java.net.URL;
-import java.util.concurrent.atomic.AtomicLong;
-
-import com.fasterxml.jackson.core.JsonParser;
-import com.sun.deploy.net.HttpResponse;
+import com.fasterxml.jackson.databind.util.JSONWrappedObject;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import sun.net.www.http.HttpClient;
+
+import javax.json.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.net.URL;
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * Created by Laora on 02/04/2017.
@@ -31,13 +28,22 @@ public class GreetingController {
         String types ="food";
         String key="AIzaSyDYuot7UKUyjnymjMt9M2KoyHSmqg_JTzM";
         String placeString = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="+lattitude+","+longitude+"&radius="+radius+"&types="+types+"&key="+key;
-
         try {
             URL url = new URL(placeString);
             BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
             String strTemp = "";
+            String jsonString = "";
             while (null != (strTemp = br.readLine())) {
-                System.out.println(strTemp);
+                jsonString = jsonString+"\n"+strTemp;
+            }
+            JsonReader reader = Json.createReader(new StringReader(jsonString));
+            JsonObject placeObject = reader.readObject();
+            reader.close();
+
+            JsonArray results = placeObject.getJsonArray("results");
+            for (JsonValue jsonValue : results) {
+                JsonObject resultsObject = (JsonObject) jsonValue;
+                System.out.println(resultsObject.getString("name"));
             }
         } catch (Exception ex) {
             ex.printStackTrace();
