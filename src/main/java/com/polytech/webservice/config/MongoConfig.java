3c9fb87b6b2f36c129947c7e0487ade64c3cefeb
Cyprien <cypconnet@wanadoo.fr>
2017-04-10 16:33:46 +0200

    ---------------V27---------------

--- src/main/java/com/polytech/webservice/config/MongoConfig.java
+++ src/main/java/com/polytech/webservice/config/MongoConfig.java
@@ -2,6 +2,7 @@ package com.polytech.webservice.config;
 
 import com.mongodb.Mongo;
 import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -44,7 +45,8 @@ public class MongoConfig {
 
     @Bean
     public Mongo mongo() throws Exception {
-        return new MongoClient("ds157500.mlab.com", 57500);
+        MongoClientURI mongoClientURI = new MongoClientURI("mongodb://<dbuser>:<dbpassword>@ds157500.mlab.com:57500/heroku_stzl8xkw");
+        return new MongoClient(mongoClientURI);
     }
 
     @Bean
