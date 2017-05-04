62c5e4281aac21e9e813be6e6d74338884d9c08c
Cyprien <cypconnet@wanadoo.fr>
2017-05-04 15:37:11 +0200

    ---------------V1.1.0--------------- Recherche avancée presque terminée !

--- src/main/java/com/polytech/webservice/web/GreetingController.java
+++ src/main/java/com/polytech/webservice/web/GreetingController.java
@@ -38,7 +38,6 @@ public class GreetingController {
                                      @RequestParam(value="search", defaultValue = "null", required = false) String search, @RequestParam(value="pref", defaultValue="null", required = false) String pref,
                                      @RequestParam(value="rayon", defaultValue="null", required = false) String rayon, @RequestParam(value="types", defaultValue="null", required = false) String types,
                                      @RequestParam(value="open", defaultValue="null", required = false) String openNow) {
-        repositoryS.deleteAll();
 
         int compteurGoogleRequest = 0;
         //Requête API Météo
