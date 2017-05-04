6bc811226e8bd2d81dbf8795108cb585824f6153
Cyprien <cypconnet@wanadoo.fr>
2017-05-04 09:46:05 +0200

    ---------------V1.1.0--------------- Recherche avancée presque terminée !

--- src/main/java/com/polytech/webservice/web/GreetingController.java
+++ src/main/java/com/polytech/webservice/web/GreetingController.java
@@ -124,7 +124,7 @@ public class GreetingController {
                     else{
                         placeString = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + latitude + "," + longitude + "&radius=" +rayon ;
                         if (!types.equals("null")){
-                            String arrayTypes[] = types.split("=");
+                            String arrayTypes[] = types.split("%");
                             String typesGoogle = "";
                             for (int i = 0; i < arrayTypes.length; i++){
                                 typesGoogle = typesGoogle + arrayTypes[i] + '|';
